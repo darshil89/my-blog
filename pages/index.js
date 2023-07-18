457ee47367a0499ec2de76d73fc0a82c0f387edf
darshil89 <darshilmahraur67@gmail.com>
2023-07-18 23:02:33 +0530

    all work done except content page

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,61 @@
 import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/post-util";
 
 const DUMMY_POSTS = [
-  {slug :'getting-started-with-nextjs' ,title:'getting-started-with-nextjs' , image:'getting-started-nextjs.png' ,
-  excerpt:"The React Framework for the Web enables you to create full-stack Web applications" ,
-  date:'2023-02-16'},
-  {slug :'getting-started-with-nextjs1' ,title:'getting-started-with-nextjs1' , image:'getting-started-nextjs.png' ,
-  excerpt:"The React Framework for the Web enables you to create full-stack Web applications" ,
-  date:'2023-02-16'},
-  {slug :'getting-started-with-nextjs2' ,title:'getting-started-with-nextjs2' , image:'getting-started-nextjs.png' ,
-  excerpt:"The React Framework for the Web enables you to create full-stack Web applications" ,
-  date:'2023-02-16'},
-  {slug :'getting-started-with-nextjs3' ,title:'getting-started-with-nextjs3' , image:'getting-started-nextjs.png' ,
-  excerpt:"The React Framework for the Web enables you to create full-stack Web applications" ,
-  date:'2023-02-16'}
-]
+  {
+    slug: "getting-started-with-nextjs",
+    title: "getting-started-with-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "The React Framework for the Web enables you to create full-stack Web applications",
+    date: "2023-02-16",
+  },
+  {
+    slug: "getting-started-with-nextjs1",
+    title: "getting-started-with-nextjs1",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "The React Framework for the Web enables you to create full-stack Web applications",
+    date: "2023-02-16",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "getting-started-with-nextjs2",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "The React Framework for the Web enables you to create full-stack Web applications",
+    date: "2023-02-16",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "getting-started-with-nextjs3",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "The React Framework for the Web enables you to create full-stack Web applications",
+    date: "2023-02-16",
+  },
+];
 
-
-function Homepage() {
-
+function Homepage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS}/>
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    
+  };
 }
 
 export default Homepage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,5 @@
 import AllPosts from "../../components/posts/all-posts";
+import {getAllPosts} from '../../lib/post-util';
 const DUMMY_POSTS = [
     {slug :'getting-started-with-nextjs' ,title:'getting-started-with-nextjs' , image:'getting-started-nextjs.png' ,
     excerpt:"The React Framework for the Web enables you to create full-stack Web applications" ,
@@ -13,8 +14,16 @@ const DUMMY_POSTS = [
     excerpt:"The React Framework for the Web enables you to create full-stack Web applications" ,
     date:'2023-02-16'}
   ]
-function AllPostsPage() {
-    return <AllPosts posts={DUMMY_POSTS}/>
+function AllPostsPage(props) {
+    return <AllPosts posts={props.posts}/>
 }
+export function getStaticProps(){
+    const allPosts=getAllPosts();
+    return {
+        props:{
+            posts:allPosts
+        }
+    }
 
+}
 export default AllPostsPage;
