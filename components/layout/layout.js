8a099c0a5697a0482024137c4fe2fd3b309aa670
darshil89 <darshilmahraur67@gmail.com>
2023-07-18 10:16:48 +0530

    layout work done without logo and styling

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
+
+export default Layout;
