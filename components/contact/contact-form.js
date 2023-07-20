b10d83c4363b6cb1d4e48283a6c29c7a9005f395
darshil89 <darshilmahraur67@gmail.com>
2023-07-20 09:13:35 +0530

    contact page build successfully

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,68 @@
 import classes from "./contact-form.module.css";
-
+import { useState } from "react";
 function ContactForm() {
-  return <section className={classes.contact}>
-    <h1>How can I help you?</h1>
-    <form className={classes.form}>
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+    // fetch() is a built-in browser API
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({ email, name, message }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setEmail("");
+    setName("");
+    setMessage("");
+    alert("Message Sent Successfully")
+  }
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
-            <div className={classes.control}>
-                <label htmlFor="email">Your Email</label>
-                <input type="email" id="email" required />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" required />
-            </div>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
         </div>
         <div className={classes.control}>
-            <label htmlFor="message">Your Message</label>
-            <textarea id="message" rows="5"></textarea>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
-            <button>Send Message</button>
+          <button>Send Message</button>
         </div>
-    </form>
-  </section>;
+      </form>
+    </section>
+  );
 }
 
 export default ContactForm;
